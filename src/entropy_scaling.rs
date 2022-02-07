f419b80fa003778bfb642f36aa26eadf856fb20b
bursik <benjamin.bursik@itt.uni-stuttgart.de>
2022-02-07 17:49:42 +0100

    working on entropy scaling implementation

--- src/entropy_scaling.rs
+++ src/entropy_scaling.rs
@@ -1,13 +1,17 @@
+use std::rc::Rc;
+
 use crate::fundamental_measure_theory::{FMTContribution, FMTProperties, FMTFunctional};
 use crate::profile::DFTProfile;
 use crate::{
     ConvolverFFT, FunctionalContribution, HelmholtzEnergyFunctional, WeightFunction,
-    WeightFunctionInfo, WeightFunctionShape,
+    WeightFunctionInfo, WeightFunctionShape, Convolver,
 };
 use feos_core::{Contributions, EosResult, EosUnit};
-use ndarray::{Array, Dimension, Axis, RemoveAxis};
-use num_dual::Dual64;
+use ndarray::{Array, Dimension, Axis, RemoveAxis, Ix1, Array1, Array2};
+use num_dual::{Dual64, DualVec};
 use quantity::{QuantityArray, QuantityScalar};
+use quantity::si::*;
+
 
 pub trait EntropyScalingFunctionalContribution: FunctionalContribution {
     fn weight_functions_entropy(&self, temperature: f64) -> WeightFunctionInfo<f64>;
@@ -24,14 +28,47 @@ impl<P: FMTProperties> EntropyScalingFunctionalContribution for FMTContribution<
 }
 
 
-impl EntropyScalingFunctionalContribution for FMTFunctional {
-    fn weight_functions_entropy(&self, temperature: f64) -> WeightFunctionInfo<f64> {
-        let r = self.properties.hs_diameter(temperature) * 0.5;
-        WeightFunctionInfo::new(self.properties.component_index(), false).add(
-            WeightFunction::new_scaled(r, WeightFunctionShape::Theta),
-            true,
-        )
+// impl EntropyScalingFunctionalContribution for FMTFunctional {
+//     fn weight_functions_entropy(&self, temperature: f64) -> WeightFunctionInfo<f64> {
+//         let r = self.properties.hs_diameter(temperature) * 0.5;
+//         WeightFunctionInfo::new(self.properties.component_index(), false).add(
+//             WeightFunction::new_scaled(r, WeightFunctionShape::Theta),
+//             true,
+//         )
+//     }
+// }
+
+
+impl<U: EosUnit> EntropyScalingFunctional<U> for FMTFunctional {
+    fn entropy_scaling_contributions(&self) -> &[Box<dyn EntropyScalingFunctionalContribution>]{
+        unimplemented!();
     }
+
+    fn viscosity_reference<D>(
+        &self,
+        density: &QuantityArray<U, D::Larger>,
+        temperature: QuantityScalar<U>,
+    ) -> EosResult<QuantityArray<U, D>>
+    where
+        D: Dimension,
+        D::Larger: Dimension<Smaller = D>{
+            unimplemented!();
+        }
+
+    /// Correlation function for entropy scaling of the shear viscosity.
+    fn viscosity_correlation<D>(
+        &self,
+        s_res: &Array<f64, D>,
+        density: &QuantityArray<U, D::Larger>,
+    ) -> EosResult<Array<f64, D>>
+    where
+        D: Dimension,
+        D::Larger: Dimension<Smaller = D>{
+            unimplemented!();
+        }
+
+
+
 }
 
 pub trait EntropyScalingFunctional<U: EosUnit>: HelmholtzEnergyFunctional {
@@ -78,16 +115,24 @@ pub trait EntropyScalingFunctional<U: EosUnit>: HelmholtzEnergyFunctional {
     //     D::Larger: Dimension<Smaller = D>;
 }
 
-impl<U, D, F> DFTProfile<U, D, F>
+// impl<F,D> DFTProfile<SIUnit, D, F>
+// where
+// D: Dimension,
+//     F: HelmholtzEnergyFunctional + EntropyScalingFunctional<SIUnit>,
+// pub fn viscosity_profile(&self){
+//     let density = self.density 
+// }
+
+
+
+
+impl<F> DFTProfile<SIUnit, Ix1, F>
 where
-    U: EosUnit,
-    D: Dimension,
-    D::Larger: Dimension<Smaller = D>,
-    F: HelmholtzEnergyFunctional + EntropyScalingFunctional<U>,
+    F: HelmholtzEnergyFunctional + EntropyScalingFunctional<SIUnit>,
 {
-    pub fn viscosity_profile(&self) -> EosResult<QuantityArray<U, D>> {
-        let temperature_red = self.temperature.to_reduced(U::reference_temperature())?;
-        let density_red = self.density.to_reduced(U::reference_density())?;
+    pub fn viscosity_profile_1d(&self) -> EosResult<SIArray<Ix1>> {
+        let temperature_red = self.temperature.to_reduced(SIUnit::reference_temperature())?;
+        let density_red = self.density.to_reduced(SIUnit::reference_density())?;
 
         let weight_functions_dual: Vec<WeightFunctionInfo<Dual64>> = self
             .dft
@@ -97,7 +142,7 @@ where
             .map(|c| c.weight_functions(Dual64::from(temperature_red).derive()))
             .collect();
 
-        let convolver_dual = ConvolverFFT::plan(&self.grid, &weight_functions_dual, None);
+            let convolver_dual:Rc<dyn Convolver<DualVec<f64, f64,1>, Ix1>> = ConvolverFFT::plan(&self.grid, &weight_functions_dual, None);
 
         // Initialize entropy convolver
         let functional_contributions_entropy = self.dft.functional.entropy_scaling_contributions();
@@ -106,7 +151,7 @@ where
                 .iter()
                 .map(|c| c.weight_functions_entropy(temperature_red))
                 .collect();
-        let convolver_entropy = ConvolverFFT::plan(
+        let convolver_entropy:Rc<dyn Convolver<f64, Ix1>> = ConvolverFFT::plan(
             &self.grid,
             &weight_functions_entropy,
             None,
@@ -116,13 +161,13 @@ where
         // Code (originally placed in entropy_density_contributions)
 
         // Weighted densities
-        let weighted_densities_entropy = convolver_entropy.weighted_densities(&density_red);
+        let weighted_densities_entropy:Vec<Array2<f64>> = convolver_entropy.weighted_densities(&density_red);
 
 
             // Molar entropy calculation for each contribution
         let entropy_molar_contributions = self
             .dft
-            .entropy_density_contributions(
+            .entropy_density_contributions::<Ix1>(
                 temperature_red,
                 &density_red,
                 &convolver_dual,
@@ -131,25 +176,25 @@ where
             .iter()
             .zip(weighted_densities_entropy.iter())
             .map(|(v, w)| {
-                (v * U::reference_volume().powi(-1)
-                    / (&w.index_axis(Axis(0), 0) * U::reference_density()))
-                    // / (&w.slice(s![0, ..]) * U::reference_density()))
-                .to_reduced(U::reference_moles().powi(-1))
+                (v * SIUnit::reference_volume().powi(-1)
+                    / (&w.index_axis(Axis(0), 0) * SIUnit::reference_density()))
+                    // / (&w.slice(s![0, ..]) * SIUnit::reference_density()))
+                .to_reduced(SIUnit::reference_moles().powi(-1))
                 .unwrap()
             })
             .collect::<Vec<_>>();
 
 
-        let mut dim = vec![];
-        self.density.shape().iter().skip(0).for_each(|&d| dim.push(d));
-        let mut entropy_molar = Array::zeros(dim);
-        // let mut entropy_molar = Array::zeros(self.density.raw_dim().remove_axis(Axis(0)));
+        // let mut dim = vec![];
+        // self.density.shape().iter().skip(0).for_each(|&d| dim.push(d));
+        // let mut entropy_molar = Array1::zeros(dim);//.into_dimensionality().unwrap();
+        let mut entropy_molar = Array::zeros(self.density.raw_dim().remove_axis(Axis(0)));
         for contr in entropy_molar_contributions.iter() {
             entropy_molar += contr;
         }
 
         // 
-        // entropy_molar = entropy_molar * U::reference_moles().powi(-1);
+        // entropy_molar = entropy_molar * SIUnit::reference_moles().powi(-1);
         let s_res = entropy_molar.mapv(|s| f64::min(s, 0.0));
 
       
@@ -163,17 +208,18 @@ where
         //         &self.convolver,
         //         Contributions::Residual,
         //     )?
-        //     .to_reduced(U::reference_moles().powi(-1))?
+        //     .to_reduced(SIUnit::reference_moles().powi(-1))?
         //     .mapv(|v| f64::min(v, 0.0));
 
         let visc_ref = self
             .dft
             .functional
-            .viscosity_reference(&self.density, self.temperature)
+            .viscosity_reference::<Ix1>(&self.density, self.temperature)
             .unwrap();
 
         // let mut viscosity_shear = Array::zeros(entropy_molar.raw_dim());
-        let mut viscosity_shear = self.dft.functional.viscosity_correlation(&s_res, &self.density).unwrap().mapv(f64::exp) * visc_ref.to_reduced(U::reference_viscosity())?;
+        // let density = self.density;
+        let mut viscosity_shear = self.dft.functional.viscosity_correlation::<Ix1>(&s_res, &self.density).unwrap().mapv(f64::exp) * visc_ref.to_reduced(SIUnit::reference_viscosity())?;
 
 
         // viscosity_shear.slice_mut(s![2..-2]).assign( 
@@ -183,11 +229,11 @@ where
         //         .viscosity_correlation::<Ix1>(&s_res, density)
         //         .unwrap()
         //         .mapv(f64::exp)
-        //         * visc_ref.to_reduced(U::reference_viscosity())?),
+        //         * visc_ref.to_reduced(SIUnit::reference_viscosity())?),
         // );
 
 
-        Ok(viscosity_shear * U::reference_viscosity())
+        Ok(viscosity_shear * SIUnit::reference_viscosity())
     
     }
 }
